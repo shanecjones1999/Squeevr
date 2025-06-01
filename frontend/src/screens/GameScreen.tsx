e74234a05fc67635e6400039ece88eb82962f14b
Shane Jones <shane@Shanes-MacBook-Pro.local>
2025-06-01 09:01:14 -0400

    Game Screen Updates

--- frontend/src/screens/GameScreen.tsx
+++ frontend/src/screens/GameScreen.tsx
@@ -1,40 +1,38 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { useGameStore } from "../store/gameStore";
-import GameOver from "../components/game/GameOver";
-// import { usePeerConnection } fro "../hooks/usePeerConnection";
-import PlayerGameScreen from "./PlayerGameScreen";
-import TvGameScreen from "./TvGameScreen";
-import PlayerList from "../components/game/PlayerList";
+// import React from "react";
+// import { motion } from "framer-motion";
+// import { useGameStore } from "../store/gameStore";
+// import GameOver from "../components/game/GameOver";
+// import TvGameScreen from "./TvGameScreen";
+// import PlayerList from "../components/game/PlayerList";
 
-const GameScreen: React.FC = () => {
-    const {
-        isHost,
-        gameState,
-        players,
-        startGame,
-        resetGame,
-        leaveRoom,
-        clientId,
-    } = useGameStore();
+// const GameScreen: React.FC = () => {
+//     const {
+//         isHost,
+//         gameState,
+//         players,
+//         startGame,
+//         resetGame,
+//         leaveRoom,
+//         clientId,
+//     } = useGameStore();
 
-    return (
-        <motion.div
-            className="w-full h-full"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-        >
-            <div className="h-full flex flex-col">
-                {gameState.gameStarted && <TvGameScreen />}
+//     return (
+//         <motion.div
+//             className="w-full h-full"
+//             initial={{ opacity: 0 }}
+//             animate={{ opacity: 1 }}
+//             exit={{ opacity: 0 }}
+//             transition={{ duration: 0.3 }}
+//         >
+//             <div className="h-full flex flex-col">
+//                 {gameState.gameStarted && <TvGameScreen />}
 
-                {gameState.eliminated && (
-                    <GameOver onRestart={resetGame} onExit={leaveRoom} />
-                )}
-            </div>
-        </motion.div>
-    );
-};
+//                 {gameState.eliminated && (
+//                     <GameOver onRestart={resetGame} onExit={leaveRoom} />
+//                 )}
+//             </div>
+//         </motion.div>
+//     );
+// };
 
-export default GameScreen;
+// export default GameScreen;
